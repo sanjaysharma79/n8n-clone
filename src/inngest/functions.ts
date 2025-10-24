bea7ead739770840284f7b0d12cc5f45e6b7edc8
Sanjay Sharma <sanjay.ks.sharma@gmail.com>
2025-10-24 18:56:23 +0530

    Error Tracking using Sentry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,9 +3,9 @@ import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
 import { generateText } from "ai";
+import * as Sentry from "@sentry/nextjs";
 
 // const google = createGoogleGenerativeAI({ apiKey: process.env.GOOGLE_API_KEY, }); // default key name -> GOOGLE_GENERATIVE_AI_API_KEY
-
 const google = createGoogleGenerativeAI();
 const openai = createOpenAI();
 const anthropic = createAnthropic();
@@ -16,6 +16,12 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
 
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+    console.warn("User triggered test log");
+    console.error("This is a dummy Error I want to track..");
+
     const { steps: geminiSteps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText,
@@ -23,6 +29,11 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.0-flash-lite"),
         system: "You are a helpful assistant",
         prompt: "what is 2+5?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
@@ -33,6 +44,11 @@ export const execute = inngest.createFunction(
         model: openai("gpt-3.5-turbo"),
         system: "You are a helpful assistant",
         prompt: "what is 2+5?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
@@ -43,6 +59,11 @@ export const execute = inngest.createFunction(
         model: anthropic("claude-3-7-sonnet-latest"),
         system: "You are a helpful assistant",
         prompt: "what is 2+5?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
